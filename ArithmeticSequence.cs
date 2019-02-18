848ef7c3f0935602a97db3f10071c9d95ec1e0c6
mikilanjlo <mikilanjlo@yandex.by>
2019-02-18 14:39:03 +0300

    calculate

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -6,8 +6,21 @@ namespace PadawansTask2
     {
         public static int Calculate(int number, int add, int count)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (add <= 0)
+                throw new ArgumentException("add <= 0");
+            if (count <= 0)
+                throw new ArgumentException("count <= 0");
+            int result = 0;
+            try
+            {
+                for (int i = 0; i < count; i++)
+                    result += number + add * i;
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return result;
         }
     }
 }
